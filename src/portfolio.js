b527a416fa105bb290bf6a95577e7c37d14b199d
Kanav chauhan <kanavchauhan23@gmail.com>
2026-02-15 15:12:16 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -112,7 +112,7 @@ const educationInfo = {
   schools: [
     {
       schoolName: "Chandigarh Group of Colleges, Landran",
-      logo: require("./assets/images/collegeLogo.png"), // Replace with your college logo
+      logo: require("./assets/images/cgclogocircle.png"), // Replace with your college logo
       subHeader: "B.Tech in Computer Science Engineering",
       duration: "2022 - 2026",
       desc: "Focused on AI, Web Development, and Cloud Computing.",
